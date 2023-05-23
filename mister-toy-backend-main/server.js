9f7ad4668360696ab0a93aa2cc63ac0cd929b51b
shaulrosenberg <shaulr10@gmail.com>
2023-05-24 00:03:15 +0300

    updated server.js for deployment

--- mister-toy-backend-main/server.js
+++ mister-toy-backend-main/server.js
@@ -7,17 +7,27 @@ const cookieParser = require('cookie-parser')
 const path = require('path')
 const cors = require('cors')
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.resolve(__dirname, 'public')))
+} else {
+    const corsOptions = {
+        origin: ['http://127.0.0.1:3000', 'http://localhost:3000'],
+        credentials: true
+    }
+    app.use(cors(corsOptions))
+}
+
 
 // App Configuration
-const corsOptions = {
-    origin: [
-        'http://127.0.0.1:8080',
-        'http://localhost:8080',
-        'http://127.0.0.1:3000',
-        'http://localhost:3000'
-    ],
-    credentials: true
-}
+// const corsOptions = {
+//     origin: [
+//         'http://127.0.0.1:8080',
+//         'http://localhost:8080',
+//         'http://127.0.0.1:3000',
+//         'http://localhost:3000'
+//     ],
+//     credentials: true
+// }
 app.use(cors(corsOptions))
 app.use(express.static('public'))
 app.use(cookieParser()) // for res.cookies
@@ -134,4 +144,13 @@ app.delete('/api/toy/:toyId', (req, res) => {
 
 
 // Listen will always be the last line in our server!
-app.listen(3030, () => console.log('Server listening on port 3030!'))
+const port = process.env.PORT || 3030;
+
+app.get('/**', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+})
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}!`)
+    console.log('Hello render.com')
+});
